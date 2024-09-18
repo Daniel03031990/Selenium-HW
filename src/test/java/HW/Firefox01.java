405ed27f76190ea66ce83d2b891257129c25a01d
Daniel03031990 <grachiladaniel@gmail.com>
2024-09-18 19:34:29 +0100

    Hw till Css

--- src/test/java/HW/Firefox01.java
+++ src/test/java/HW/Firefox01.java
@@ -1,10 +1,9 @@
 package HW;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Ex01 {
+public class Firefox01 {
     public static void main(String[] args) {
 
 
---
+++ src/test/java/HW/Xpath04.java
@@ -0,0 +1,54 @@
+package HW;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath04 {
+    public static void main(String[] args) {
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.syntaxprojects.com/Xpath-homework.php");
+
+       WebElement hobbies=driver.findElement(By.xpath("//input[@id='yourHobbiesId']"));
+
+        hobbies.sendKeys("Boxing");
+
+        WebElement ClickButton=driver.findElement(By.xpath("//button[text()='Click Here']"));
+        ClickButton.click();
+
+        WebElement favouriteMovie=driver.findElement(By.xpath("//input[@id='favoriteMoviesId']"));
+        favouriteMovie.sendKeys("Lord of the Rings");
+
+        WebElement text=driver.findElement(By.xpath("//label[contains(text(),'Lorem ipsum dolor sit')]"));
+        String textOff=text.getText();
+        System.out.println(textOff);
+
+        WebElement city=driver.findElement(By.xpath("//input[@id='yourCity']"));
+        city.sendKeys("Boston");
+
+        WebElement ProfesionalEmail=driver.findElement(By.xpath("(//input[@class='form-control backup' ])[3]"));
+        ProfesionalEmail.sendKeys("");
+
+        WebElement name= driver.findElement(By.xpath("//input[@id='clientName' and @class='form-control']"));
+        name.sendKeys("Gregory");
+
+       WebElement Id=driver.findElement(By.xpath("//input[@name='clientId' and @class='form-control']"));
+       Id.sendKeys("12452455");
+
+       WebElement StreetNr=driver.findElement(By.xpath("//input[@name='StreetNo']"));
+       StreetNr.sendKeys("25");
+
+      WebElement houseNr=driver.findElement(By.xpath("//input[@name='HouseNo']"));
+      houseNr.sendKeys("12");
+
+
+
+
+
+    }
+}
